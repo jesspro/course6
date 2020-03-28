61c89f53a6cf1e01bec337d381790c3bdab10eea
jesspro <8378240+jesspro@users.noreply.github.com>
2020-03-28 23:51:12 +0000

    added files to be committed

---
+++ changeImage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#you will be using the PIL library to update all images within
+#~/supplier-data/images directory
+#size: change resol. to 600x400
+#format: change from .tiff to .jpeg
+
+#after processing the images, save them in ~/supplier-data/images
+#with JPEG extension
+
+
