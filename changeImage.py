fe9b11dbb82cd9865b2b96ebbe28a770486c87cc
jesspro <8378240+jesspro@users.noreply.github.com>
2020-03-29 01:01:31 +0000

    updated changeImage.py

--- changeImage.py
+++ changeImage.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+from PIL import Image
+
+import glob,os
+
+size = 600, 400
+
+for infile in glob.glob("/supplier-data/images/*.tiff"):
+    im = Image.open(infile).convert("RGB")
+    im.resize((size)).save("/supplicer-data/images/" + infile + ".JPEG", "JPEG")
+
+
+
 #you will be using the PIL library to update all images within
 #~/supplier-data/images directory
 #size: change resol. to 600x400
