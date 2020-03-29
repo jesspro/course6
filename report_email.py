babb9df55f47d192fce72870bf84a1bbecd40475
jesspro <8378240+jesspro@users.noreply.github.com>
2020-03-29 16:00:43 +0000

    added template for report_email.py

---
+++ report_email.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# import all the necessary libraries(os, datetime and reports)
+# that will be used to process the text data from the
+# supplier-data/descriptions directory into the format below:
+
+# name: Apple
+#
+# weight: 500 lbs
+#
+# [blank line]
+#
+# name: Avocado
+#
+# weight: 200 lbs
+#
+# [blank line]
+#
+# Once you have completed this, call the main method which will
+# process the data and call the generate_report method from the 
+# reports module:
+
+# if __name__ == "__main__":
+
+# You will need to pass the following arguments to the
+# reports.generate_report method: the text description processed
+# from the text files as the paragraph argument, the report title
+# as the title argument, and the file path of the PDF to be
+# generated as the attchment argument (use "/tmp/processed.pdf")
+
+# reports.generate_report(attachment, title, paragraph)
+
+
