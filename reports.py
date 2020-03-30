2ed678a38986e77c286a7b9478cef7c805cba8ab
jesspro <378240+jesspro@users.noreply.github.com>
2020-03-29 22:30:14 -0400

    added spacer between fruits in reports.py

--- reports.py
+++ reports.py
@@ -6,9 +6,17 @@ from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
 
 def generate_report(attachment, title, paragraph):
+
     styles = getSampleStyleSheet()
     report = SimpleDocTemplate(attachment)
-    report_title = Paragraph(title, styles["h1"])
-    report_info = Paragraph(paragraph, styles["BodyText"])
+    report_title = Paragraph(title, styles["H1"])
+    #report_info = Paragraph(paragraph, styles["BodyText"])
     empty_line = Spacer(1,20)
-    report.build([report_title, empty_line, report_info])
+
+    report_structure = [report_title, empty_line]
+    fruits = paragraph.split("<br /><br />")
+    for fruit in fruits:
+        report_info = Paragraph(fruit, styles["BodyText"])
+        report_structure.append(report_info)
+        report_structure.append(empty_line)    
+    report.build([report_structure])
